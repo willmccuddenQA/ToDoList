cd7e761142c143f11440ce1ba4de266004b8587b
Will McCudden <wmccudden-hughes@qa.com>
2021-02-04 15:58:10 +0000

    ItemDomain class completed

--- src/main/java/com/qa/persistence/domain/ItemDomain.java
+++ src/main/java/com/qa/persistence/domain/ItemDomain.java
@@ -1,5 +1,74 @@
 package com.qa.persistence.domain;
 
-public class ItemDomain {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+@Entity
+public class ItemDomain {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	private Long id;
+	private String description;
+	private String completeBy;
+	private Boolean completeStatus;
+	private Long list_id;
+	
+	public ItemDomain(Long id, String description, String completeBy, Boolean completeStatus, Long list_id) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.completeBy = completeBy;
+		this.completeStatus = completeStatus;
+		this.list_id = list_id;
+	}
+
+	public ItemDomain() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCompleteBy() {
+		return completeBy;
+	}
+
+	public void setCompleteBy(String completeBy) {
+		this.completeBy = completeBy;
+	}
+
+	public Boolean getCompleteStatus() {
+		return completeStatus;
+	}
+
+	public void setCompleteStatus(Boolean completeStatus) {
+		this.completeStatus = completeStatus;
+	}
+
+	public Long getList_id() {
+		return list_id;
+	}
+
+	public void setList_id(Long list_id) {
+		this.list_id = list_id;
+	}
+	
 }
