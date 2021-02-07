5610e634da5884cc886b58276ebf7e02955e0a23
Will McCudden <wmccudden-hughes@qa.com>
2021-02-07 13:28:18 +0000

    added properties files and sql data test file

---
+++ src/main/resources/data-test.sql
@@ -0,0 +1,11 @@
+INSERT INTO LIST_DOMAIN (title) VALUES ('Todays jobs'), ('Targets for the year'),('Shopping List');
+
+INSERT INTO ITEM_DOMAIN (COMPLETE_BY, COMPLETE_STATUS, DESCRIPTION, LIST_ID) VALUES
+('Midday',false,'Milk',3),
+('Midday',false,'Bread',3),
+('Midday',false,'Butter',3),
+('Today',true,'Tidy room',1),
+('Today',true,'Take bins out',1),
+('31st December',false,'Read 12 books',2),
+('31st December',false,'Learn to do a rubiks cube',2);
+
