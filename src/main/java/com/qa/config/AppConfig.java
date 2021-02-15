c6cbe6726e72b038d17570e4b311375a90236a4d
Will McCudden <wmccudden-hughes@qa.com>
2021-02-15 23:24:43 +0000

    all front end tests complete and mvn project built

--- src/main/java/com/qa/config/AppConfig.java
+++ src/main/java/com/qa/config/AppConfig.java
@@ -3,9 +3,16 @@ package com.qa.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Configuration
 public class AppConfig {
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+	    return new ModelAndView("index.html");
+	}
 	
 	@Bean
     public ModelMapper mapper() {
